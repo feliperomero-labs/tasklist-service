afb3e19f260ff1eebc24e9e5c9bd6aeab0700f88
feliperasilva <fras2@discente.ifpe.edu.br>
2026-04-18 11:59:28 -0300

    feat: implement Task entity with methods for completion and renaming

---
+++ src/modules/task/domain/entities/task.entity.ts
@@ -0,0 +1,36 @@
+export class Task {
+    constructor(
+        private readonly id: string,
+        private title: string,
+        private completed: boolean,
+        private createat: Date,
+    ) {}
+
+    complete() {
+        if (this.completed) {
+            throw new Error('Task is already completed');
+        }
+
+        this.completed = true;
+    }
+
+    rename(newTitle: string) {
+        if (newTitle.length < 3) {
+            throw new Error('Title must be at least 3 characters long');
+        }
+        
+        this.title = newTitle;
+    }
+
+    getId() {
+        return this.id;
+    }
+    
+    getTitle() {
+        return this.title;
+    }
+
+    isCompleted() {
+        return this.completed;
+    }
+}
